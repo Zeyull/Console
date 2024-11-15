3976b962158b25f3cdf93d28f09b1827aa13e936
nozomi <13508085664@163.com>
2024-11-16 01:19:40 +0800

    feat: 使用颜色选择器替代input输入框

--- src/pages/AnnouncementManage/components/CreateAnnouncement.tsx
+++ src/pages/AnnouncementManage/components/CreateAnnouncement.tsx
@@ -10,12 +10,20 @@ import {
   DrawerForm,
   ProFormDatePicker,
   ProFormInstance,
+  ProFormItem,
 } from '@ant-design/pro-components';
 import { useIntl } from '@umijs/max';
-import { Button, message } from 'antd';
+import { Button, ColorPicker, ColorPickerProps, message, theme } from 'antd';
+import { generate, green, presetPalettes, red } from '@ant-design/colors';
 import { forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react';
 
+type Presets = Required<ColorPickerProps>['presets'][number];
+const genPresets = (presets = presetPalettes) =>
+  Object.entries(presets).map<Presets>(([label, colors]) => ({ label, colors }));
+
 export default forwardRef(function CreateAnnouncement({ tableRef }: { tableRef: any }, ref: any) {
+  const { token } = theme.useToken();
+  const presets = genPresets({ primary: generate(token.colorPrimary), red, green });
   const formRef = useRef<ProFormInstance>();
   const [drawerStatus, setDrawerStatus] = useState(false);
   const [announcementId, setAnnouncementId] = useState(-1);
@@ -98,6 +106,8 @@ export default forwardRef(function CreateAnnouncement({ tableRef }: { tableRef: 
         open={drawerStatus}
         onOpenChange={setDrawerStatus}
         onFinish={async (values: any) => {
+          const color = values.color.toHexString();
+          values.color = color;
           if (announcementId === -1) {
             const createResult = await createAnnouncementFn(values);
             if (createResult) {
@@ -130,13 +140,18 @@ export default forwardRef(function CreateAnnouncement({ tableRef }: { tableRef: 
           />
         </ProForm.Group>
         <ProForm.Group>
-          <ProFormText
+          <ProFormItem
             name="color"
-            width="md"
             required={true}
             rules={[{ required: true }]}
             label={intl.formatMessage({ id: 'pages.announcementManage.color.label' })}
-          />
+          >
+            <ColorPicker
+              presets={presets}
+              showText={(color) => <span>{color.toHexString().slice(0, 7)}</span>}
+              disabledAlpha
+            ></ColorPicker>
+          </ProFormItem>
           <ProFormText
             width="md"
             name="icon"
